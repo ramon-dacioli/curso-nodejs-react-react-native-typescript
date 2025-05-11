43c69a93bd84f12d9880b93964dc1df9d9e6d7f9
root <ramon_muz@hotmail.com>
2025-05-11 17:13:38 -0300

    Add ex's escrever no html e inserir imagem

---
+++ Exemplo 03 - Escrever no html/script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Qual seu nome?");
+
+var sobrenome = prompt("Qual seu sobrenome?");
+
+console.log("Olá " + nome + " " + sobrenome);
+
+document.writeln("<h1> Bem vindo(a) " + nome + "</h1>")
---
+++ Exemplo 04 - Inserir imagem no html/script.js
@@ -0,0 +1,9 @@
+var nome = prompt("Qual seu nome?");
+
+var sobrenome = prompt("Qual seu sobrenome?");
+
+console.log("Olá " + nome + " " + sobrenome);
+
+document.writeln("<h1> Bem vindo(a) " + nome + "</h1>")
+
+document.writeln("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve' />")
